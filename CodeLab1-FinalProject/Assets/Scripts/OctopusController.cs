c20b44d60a4981bfe9f0f8de9fd9de70ed5670ab
jinaroo <sohnjin@gmail.com>
2019-05-17 17:35:43 -0400

    added new project and started basic setup of final project

---
+++ CodeLab1-FinalProject/Assets/Scripts/OctopusController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OctopusController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // eat good food and continue
+        if (other.CompareTag("GoodFood"))
+        {
+            UI_Manager.instance.Score++;
+            Destroy(gameObject);
+        }
+        
+        // eat bad food and lose
+        if (other.CompareTag("BadFood"))
+        {            
+            // lock player controls
+            // have lose message pop up
+        }
+    }
+}
---
+++ CodeLab1-FinalProject/Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Manager : MonoBehaviour
+{
+    public static UI_Manager instance;
+
+    public Text scoreText;
+    public Text highScoreText;
+    
+    // saving high scores (player preferences)
+    private const string PLAYER_PREF_HIGHSCORE = "highScore";
+    private int score = 0;
+    private int highScore = 0;
+
+    public int Score // property
+    {
+        get { return score; }
+        set
+        {
+            score = value;
+            scoreText.text = "score: " + score;
+
+            HighScore = score;
+        }
+    }
+
+    public int HighScore
+    {
+        get { return highScore; }
+        set
+        {
+            if (value > highScore)
+            {
+                highScore = value;
+                highScoreText.text = "high score: " + highScore;
+
+                PlayerPrefs.SetInt(PLAYER_PREF_HIGHSCORE, highScore);
+            }
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+        // cont. high scores
+        Score = 0;
+        HighScore = PlayerPrefs.GetInt(PLAYER_PREF_HIGHSCORE, 10); // default value
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
